2a6631b5c178cbf766068d2b5a12b3906b372166
Michael <michaelbuttler23@gmail.com>
2016-03-30 07:48:20 -0400

    Create ticker.php

---
+++ ticker.php
@@ -0,0 +1,22 @@
+<?php
+    $input = file_get_contents('https://c-cex.com/t/rbies-btc.json');
+    $rbies = json_decode($input);
+    $width = 150;
+    $height = 25;
+    $text = "test";//$rbies->ticker->lastprice." BTC";
+    $fontsize = 5;
+
+    $img = imagecreate($width, $height);
+
+    // Transparent background
+    $black = imagecolorallocate($img, 0, 0, 0);
+    imagecolortransparent($img, $black);
+
+    // Red text
+    $red = imagecolorallocate($img, 255, 0, 0);
+    imagestring($img, $fontsize, 0, 0, $text, $red);
+
+    header('Content-type: image/png');
+    imagepng($img);
+    imagedestroy($img);
+?>
